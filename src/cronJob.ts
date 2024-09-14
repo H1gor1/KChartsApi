f6ef56b5b10708dbe28bced0aacccdb1560fd3f5
Higor Gabriel <higorgabrieldev@gmail.com>
2024-09-14 16:32:38 -0300

    temporary fix

--- src/cronJob.ts
+++ src/cronJob.ts
@@ -1,16 +1,27 @@
 import cron from 'node-cron';
-import fetchMusicData from './Services/Flo/floService';
+import fetchFloMusicData from './Services/Flo/floService';
 import fetchGenieMusicData from './Services/Genie/genieService';
 import fetchVibeMusicData from './Services/Vibe/vibeService';
+import prisma from './prismaClient';
 
+// Função para executar as três chamadas sequencialmente
+async function fetchAllMusicDataSequentially() {
+    try {
+        await prisma.chartMusics.deleteMany();
 
-fetchGenieMusicData();
-fetchVibeMusicData();
-fetchFloMusicData();
+        await fetchGenieMusicData();
+        await fetchVibeMusicData();
+        await fetchFloMusicData();
+    } catch (error) {
+        console.error('Erro ao buscar os dados de música:', error);
+    }
+}
 
-cron.schedule('0 * * * *', ()=> {
-    
-    fetchGenieMusicData();
-    fetchVibeMusicData();
-    fetchFloMusicData();
+// Executa imediatamente na inicialização
+fetchAllMusicDataSequentially();
+
+// Agenda o cron job para rodar a cada hora
+cron.schedule('0 * * * *', async () => {
+    console.log('Executando o cron job de busca de dados de música...');
+    await fetchAllMusicDataSequentially();
 });
